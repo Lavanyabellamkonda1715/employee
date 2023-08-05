0e8ebaf4f9af84bf05234f553eb0aaf1fc60456a
Lavanyabellamkonda1715 <95727559+Lavanyabellamkonda1715@users.noreply.github.com>
2023-08-05 10:26:51 -0500

    commit code

---
+++ mysql/src/main/java/com/employee/mysql/controller/EmployeeController.java
@@ -0,0 +1,80 @@
+package com.employee.mysql.controller;
+
+
+import com.employee.mysql.model.Employee;
+import com.employee.mysql.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@Controller
+@RestController
+public class EmployeeController {
+    @Autowired
+
+    private EmployeeService employeeService;
+
+    @GetMapping("/employee")
+    public  List<Employee> getallemps()
+    {
+        return employeeService.getallemps();
+
+    }
+    @GetMapping("/employee/{id}")
+    public Employee getEmployeeById(@PathVariable("id") int id){
+        Employee employeeData =  employeeService.getEmployeeById(id);
+
+        return employeeData;
+    }
+
+    @PostMapping("/employee")
+    public void createemployee(@RequestBody Employee e ) {
+        employeeService.createemployee(e);
+
+    }
+
+//    @PutMapping("/employee/{id}")
+//    public ResponseEntity<Employee>  updateEmployee(@PathVariable("id") int id, @RequestBody Employee employee){
+//        Optional<Employee> employeeData = Optional.ofNullable(employeeService.getEmployeeById(id));
+//
+//        if(employeeData.isPresent()){
+//            Employee employeeValue = employeeData.get();
+//            employeeValue.setEmail(employee.getEmail());
+//            employeeValue.setName(employee.getName());
+//            return new ResponseEntity<>(employeeService.updateEmployee(employee), HttpStatus.OK);
+//        }
+//        else {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
+
+
+//    @GetMapping("/employees")
+//    public ResponseEntity<List<Employee>> getAllEmployee() {
+//        try {
+//            List<Employee> products = new ArrayList<>();
+//
+//            employeeService.getAllEmployees().forEach(products::add);
+//            return new ResponseEntity<>(products, HttpStatus.OK);
+//        } catch (Exception e) {
+//            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
+
+//    @GetMapping("/products/{id}")
+//    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long id) {
+//        Optional<Employee> employeeData = employeeService.getEmployeeById((int) id);
+//
+//        if (employeeData.isPresent()) {
+//            return new ResponseEntity<>(employeeData.get(), HttpStatus.OK);
+//        } else {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
+
+}
+
+
